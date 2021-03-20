f02869f04888c3e38772f5ec86e321f24d43a7fe
Manjurul Hoque Rumi <rumimanzurulhoque@gmail.com>
2021-03-20 21:53:41 +0600

    Added schedule timing form

--- doctors/urls.py
+++ doctors/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from .views import DoctorDashboardView
+from .views import DoctorDashboardView, schedule_timings
 
 app_name = "doctors"
 
 urlpatterns = [
     path("dashboard/", DoctorDashboardView.as_view(), name="dashboard"),
+    path("schedule-timings/", schedule_timings, name="schedule-timings"),
 ]
--- doctors/views.py
+++ doctors/views.py
@@ -1,8 +1,6 @@
-from django.http import HttpResponsePermanentRedirect
-from django.urls import reverse_lazy
+from django.shortcuts import render
 from django.views.generic.base import TemplateView
 
-from accounts.models import User
 from mixins.custom_mixins import DoctorRequiredMixin
 
 
@@ -12,3 +10,7 @@ class DoctorDashboardView(DoctorRequiredMixin, TemplateView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         return context
+
+
+def schedule_timings(request):
+    return render(request, 'doctors/schedule-timings.html')
