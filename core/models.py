f2c3bf00a561f40c7399349ce54a81a33b5db2ae
Manjurul Hoque Rumi <rumi@impelitsolutions.com>
2024-12-25 22:50:11 +0600

    refactor: added Speciality modell

--- core/models.py
+++ core/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class Speciality(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    image = models.ImageField(upload_to="specialities/")
+
+    def __str__(self):
+        return self.name
--- doctors/admin.py
+++ doctors/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from .models import (
+    Education,
+    Experience,
+    TimeRange,
+    Saturday,
+    Sunday,
+    Monday,
+    Wednesday,
+    Thursday,
+    Friday,
+)
 
-# Register your models here.
+admin.site.register(Education)
+admin.site.register(Experience)
+admin.site.register(TimeRange)
+admin.site.register(Saturday)
+admin.site.register(Sunday)
+admin.site.register(Monday)
+admin.site.register(Wednesday)
+admin.site.register(Thursday)
+admin.site.register(Friday)
